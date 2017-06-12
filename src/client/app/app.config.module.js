099bd9076727d14bbd221702485b2ec7f5868da3
Gonik Daniel <gonik.daniel88@gmail.com>
2017-06-12 14:23:36 +0300

    Cleaning

--- src/client/app/app.config.module.js
+++ src/client/app/app.config.module.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	angular.module('app.config', [])
-.constant('production', {"ENV":{"api":"https://gmail-clone.herokuapp.com/","development":false}})
-.constant('development', {"ENV":{"api":"http://localhost:6660/","development":true}});
+    .constant('production', {"ENV":{"api":"https://gmail-clone.herokuapp.com/","development":false}})
+    .constant('development', {"ENV":{"api":"http://localhost:6660/","development":true}});
 
 })();
